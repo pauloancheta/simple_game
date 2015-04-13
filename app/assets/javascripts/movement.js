0520190912cfc70eef704d9100907dfff4e674a6
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-04-13 15:49:24 -0700

    added images and changed append of bullets

--- app/assets/javascripts/movement.js
+++ app/assets/javascripts/movement.js
@@ -1,43 +1,42 @@
 $(document).ready(function() {
   var enemies = [$('#0'), $('#1'), $('#2'), $('#3'), $('#4'), $('#5'), $('#6'), $('#7'), $('#8'), $('#9'), $('#10')];
+  var enemy_hash = {};
 
-  var spaceship_location = function(){
-    console.log($('.spaceship').height());
-    console.log($('.spaceship').offset());
-  }
+  // var enemy_locations = function() {
+  //   for(var i in enemies){
+      
+
+  //   }
+  // }
 
   var did_enemy_hit = function(enemy){
-    var enemy_x1 = enemy.offset().left;
-    var enemy_x2 = enemy.offset().left + $('.board_coord').width();
-    var enemy_y1 = enemy.offset().top;
-    var enemy_y2 = enemy.offset().top + $('.board_coord').width() - 10;
+      var enemy_x1 = enemy.offset().left;
+      var enemy_x2 = enemy.offset().left + $('.board_coord').width();
+      var enemy_y1 = enemy.offset().top;
+      var enemy_y2 = enemy.offset().top + $('.board_coord').width() - 10;
 
-    var bullet_x1 = $('.bullet').offset().left;
-    var bullet_x2 = $('.bullet').offset().left + $('.bullet').width();
-    var bullet_y1 = $('.bullet').offset().top;
-    var bullet_y2 = $('.bullet').offset().top + $('.bullet').width();
+      var bullet_x1 = $('.bullet').offset().left;
+      var bullet_x2 = $('.bullet').offset().left + $('.bullet').width();
+      var bullet_y1 = $('.bullet').offset().top;
+      var bullet_y2 = $('.bullet').offset().top + $('.bullet').width();
 
-    enemy_hit = false
-
-    if(bullet_x1 < enemy_x2 && bullet_x1 > enemy_x1){
-      if(bullet_y1 < enemy_y2){
-        return true
+      if(bullet_x1 < enemy_x2 && bullet_x1 > enemy_x1){
+        if(bullet_y1 < enemy_y2){
+          return true;          
+        }
       }
-    }
+    
   }
 
-  var hit_event = function(trajectory, enemies){
-    for(var i = 0; i <=enemies.length; i++){
-      if( enemies[i] && did_enemy_hit(enemies[i]) ){
-        // enemies[i].css('background-color', 'red');
-        enemies[i].fadeOut(100)
+  var hit_event = function(trajectory){
+    for(var i = 0; i < enemies.length ; i++){
+      if( did_enemy_hit(enemies[i]) ){
+        $('#'+i).css('border', 'white');
+        // enemies[i].fadeOut(100)
         $('.bullet').fadeOut(100);
         clearInterval(trajectory);
-        // create another bullet
-        $('.spaceship center').append('<div class="bullet"></div>');
       }
     }
-    
   }
 
   $(this).on('keydown', function(e){
@@ -53,18 +52,16 @@ $(document).ready(function() {
     }
 
     else if(e.which == space){
-      var left = $('.bulet').css('margin-left');
+      $('.spaceship center').append('<div class="bullet"></div>');
       var trajectory = setInterval(function() {
-        $('.bullet').css('margin-left', left) // bullet trajectory is always straight
+   
         $('.bullet').css('margin-top', '-=5px');
-        hit_event(trajectory, enemies);
+        hit_event(trajectory);
       }, 5);
 
       setTimeout(function() {
         clearInterval(trajectory);
         $('.bullet').remove();
-        $('.spaceship center').append('<div class="bullet"></div>');
-        spaceship_location();
       }, 1500);
     }
   });
